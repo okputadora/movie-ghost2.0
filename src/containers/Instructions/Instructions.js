f9e6076988251482ce82a40755be126c3d114c2f
okputadora <okputadora@gmail.com>
2018-05-06 16:15:12 -0400

    added instruction page

--- src/containers/Instructions/Instructions.js
+++ src/containers/Instructions/Instructions.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import classes from './Instructions.css'
+import divider from '../../assets/divider.png';
+
 
 class Instructions extends Component{
   render(){
     return(
-      <div>INSTRUCTIONS PAGE</div>
+      <div className={classes.Instructions}>
+        <div className={classes.Header}>Instructions</div>
+        <img src={divider} height="30px"/>
+        <p className={classes.Text}>This game tests your knowledge of movies and actors</p>
+        <p className={classes.Text}>To start, one player says the name of any movie</p>
+        <p className={classes.Text}>Then the next player has to say the name of an actor in that movie</p>
+        <p className={classes.Text}>Game play continues like this, alternating between movies and actors
+        (without repeating) until one player can't think of an answer</p>
+        <p className={classes.Text}>Failure to come up with an answer results in that player getting a letter (like H O R S E
+        except we're playing G H O S T)</p>
+      </div>
     )
   }
 }
